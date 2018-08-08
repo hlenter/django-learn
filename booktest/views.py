a00edb2ca3690f3505c9c7b3ba901bbe1229aef7
Fiery_red <hlenter@163.com>
2018-08-08 23:40:29 +0800

    day01

--- booktest/views.py
+++ booktest/views.py
@@ -1,13 +1,15 @@
 # coding:utf-8
 from django.shortcuts import render
 from django.http import *
+from .models import *
+# from django.template import RequestContext, loader
 
 
 def index(request):
-    return HttpResponse('hello halo')
-
-#
-# def index(request):
-#     return HttpResponse('index')
+    # temp = loader.get_template('booktest/index.html')
+    # return HttpResponse(temp.render())
+    booklist = BookInfo.objects.all()
+    context = {'list': booklist}
+    return render(request, 'booktest/index.html', context)
 
 
